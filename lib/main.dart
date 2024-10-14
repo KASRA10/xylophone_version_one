5bff736accec1a4694da5171f6fb042f92250726
KASRA10 <kasrasparrow@gmail.comm>
2024-10-14 12:49:22 +0330

    Add Buttons as tunes with elevated button and create a function for play a sound

--- lib/main.dart
+++ lib/main.dart
@@ -8,25 +8,121 @@ void main() => runApp(
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
+  void playSound(int number) async {
+    final player = AudioPlayer();
+    await player.play(
+      AssetSource(
+        'audio/note$number.wav',
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: ElevatedButton(
-              onPressed: () async {
-                final player = AudioPlayer();
-                await player.play(
-                  AssetSource(
-                    'audio/note1.wav',
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () => playSound(1),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue[100],
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        .0,
+                      ),
+                    ),
                   ),
-                );
-              },
-              child: const Text(
-                'Click Me',
+                ),
+                child: const Text(''),
               ),
-            ),
+              ElevatedButton(
+                onPressed: () => playSound(2),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange[400],
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        .0,
+                      ),
+                    ),
+                  ),
+                ),
+                child: const Text(''),
+              ),
+              ElevatedButton(
+                onPressed: () => playSound(3),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.pink[300],
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        .0,
+                      ),
+                    ),
+                  ),
+                ),
+                child: const Text(''),
+              ),
+              ElevatedButton(
+                onPressed: () => playSound(4),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green[600],
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        .0,
+                      ),
+                    ),
+                  ),
+                ),
+                child: const Text(''),
+              ),
+              ElevatedButton(
+                onPressed: () => playSound(5),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple[300],
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        .0,
+                      ),
+                    ),
+                  ),
+                ),
+                child: const Text(''),
+              ),
+              ElevatedButton(
+                onPressed: () => playSound(6),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        .0,
+                      ),
+                    ),
+                  ),
+                ),
+                child: const Text(''),
+              ),
+              ElevatedButton(
+                onPressed: () => playSound(7),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.cyan[800],
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        .0,
+                      ),
+                    ),
+                  ),
+                ),
+                child: const Text(''),
+              ),
+            ], // End Of Children
           ),
         ),
       ),
